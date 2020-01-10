43b551bf0f623c415e7029b4f6c66a3e4fc3419c
dirkwind <dirkwind707@gmail.com>
2020-01-09 21:43:15 -0600

    add crunch.py

---
+++ crunch.py
@@ -0,0 +1,58 @@
+# custom symmetric encryption
+import random
+
+keys = {
+    0: '\'?IUû!wK░@ÚÏrþ¢9MÍ<ukZ:G¹Óà³á║ÈÁHïÎÔoËY┼J╠╬┴0i3²7X▓í└┤ªºv¶÷yð,µjça©* ø2▄êâ.úTpÂ’ÖØý█1ÆóÌ´╔╦[Õæb$Òä“·«åtÑã§ëßì6+;s╝g£B&Lé”▒═{EîmO°¿ü-¼¥┬èÊö5╚%Çh¾»¤q╗Þc½^×ôÐ4]"}=Ä¦(¯`▀│fD├ÛWÝS─zÀd╩Å¨Ã#ñNC┐QnÜù8/¦®ÙF­¸¡\ÉAR)¬õÿxV|_■~òP>e±l╣',
+    1: 'Ü×┼Úýæ]Z÷m±róz»¬╣êKú├wÉ³OªÇÍÛ╠╗ôÝ└BV1HdYg^Ó¶M­╬ï"vÁhX¨î>9¼┬¸ÿøÊxÄD│n´}(ET®kùIÒÆåÈûoÞc8áJsßÀí-ëqtÖ+Ð&”¦QØpÏã▀ò¥*G¯è©.05/²¿jLÂà¹$Ì§[)╚NWÙa¤Ã═3¢i°Î╝▄█Ñ{,\'`CÕ@Fä╦%A\Ô¡éâ?b4Uõ;ð#¦!<╔’|S2Rö6_░:=~■┐uË║ì7µ þ¾╩«f┤£Å·üPñ─▓º┴e▒“y½çl',
+    2: 'Õcç4Ñöa\';├G8h"[+Kó½¥Îàÿ1±>ÖÌ£7 SÙ¾qÏÝåñá’┐i²NB*x!v~í╚âIj┬/k|¨¢Ø·ïÅäÃrÂÆp{]┼P╠ÐÞþÓôs╣=ì5¶&æ▒n³êÚ█│:D“ÄßÔZ╦µXÛAC©0øõ.▄,Ç»uúë×└¹░Ò▓╗«RzJ?$╔û§E_yOT¤tW¦═%éQÍ´Mè┤Àm2@Ul¯L\¼b¬dgH■îª╬#Ë°¸V`®”▀¡─ÊoF9)eã^}wº-ùòüðYf­╝Á6<║¿(ÜÈÉ¦÷╩┴3ý',
+    3: '$╚┴})3╠ªgÚæ5"2a░óúeX¤│p▒’v┤Èy6ixSøÿç°Ò@ÔBEm*▓ï▄Î^=█²╬qu¦┐èl╦Ð¡ËÞ`R§oj¾&é7À¦-╗Í¹­º╣zQ íþîà~ZùwÁ>®ãA¿ÌÛ║{Ü4¬¸%N!ö/sL³nä├]µJÆÊ\▀¥(dÙM¨KØÇ.\'áåCUìÃrÑ:ûô╩ð«ß┬Å©Ä£Ö┼8|âõVP±GkÉñëY“╝1Óý└╔;t<¼■T+”─Ï¶×Âò,W_êüÕ¯¢[9hI´Fcb÷·?»½fH#Ý0═DO',
+    4: '┼ºCÊ¦êï?Àd¾]╩}csJØ│F▒Î▀3·á╗■iÉG# wû<«┴¥½ÒeÆÄ$*|”þ¼ýµ§!^’NÌa,óÂ÷h"ú¶├Xôb╝K┤ä╣Üì└DA­Å=ñVÞ(øÑPül9;ß©ÍI┬HM─~═0ÐÁ{¯5╔EL╦ð╠¢¨¹³QÚx-q®>ù&gRj27WÇæ“Õf╬t╚²▄¸4nBÝèÓÈéò×░»▓Umk¡\':YZõÖÏ¿Ù¤█£T´@1/åí%yªàpÔ¦v[_)¬┐ëÛu°±\8║SçzoãO+`.â6îËÿÃör',
+    5: '┼╦├ª/kËÑq7GÞæ1TÝ┴a´┤ê£┐.─Es╩ë¤ÛÇÉÓ9h)”M▒üâb└v█ÙÿñC~¿ZN³l▀Ö▄"OÁþÄ■╣o¬cD:»Ìº>d,¨P═iWASVû3║;ó¹j±©+╝ÐÅ®&¢LåúryÔ¼è5xØ“á{¸çù<õ╔¶Í=¾ÀY8▓Ã%┬·|Æ#öì6R Hà¦°^4Â¥g_¯2z\'0XµÒmôÎ«$IïF╚ä@ßUB([¡­-│píe½òt╗}Q÷\wÜÈn╠ãJuÏ░`Õ×îÚ²?¦K!ý╬*ø]ðfé’§Ê',
+    6: 'éöÈÀ╩z`j“@Z_ÉQ█Ì\©þá&NêôG¸(®▄ã\'ð-§¬]╚fü¯æ°┐ûùu;K«╠»T#dú±PàYmº╬ÿ? ┴ÊÍ¶k┬Lò├ß╗õo:¹÷0£I╔.aqpí/S4,vËx²Øñ3w[▒ë▓║¿╦hE╣½¼▀Ã+µX·’}A7ÜD*8─C2s^FªÞ¥BMÓ"”Önì5Á<1çól┼´ÑÙRJåÄ└ÅrÏ¢)¦ÇÕ6by³Û░|Ôc│O┤HWâtUÝ=­ge¡¦╝¤!VÎèÚ$%{i~¾ýïÂÐ9×äÆÒ■î¨>ø═',
+    7: '¤_bUÐhys½,Û©Lß\;ÔGWÅà^▒xÂj`*▄a¯|£╣dÉ4÷¼Oå¦▀Í6õºnô┴ip3¸ä!?ÿw=ïX1ùgø®’\'+f■P│├µ╗Ä█╦└[ãÃ×┬╝]âqz”ö▓æîì╬ÇòëRT¬ ­È«Üá┐─ú°ý┤Ë#¶-²Ql0§:.{Ú%þé“9$ñI/ü·ð)¦¢"Þ¹BVF}»êrÏ³╠2Ø¨ÎmÑ║Nv╔╩~Z¥oóû>uC¿ÌçM¾Õ╚èÀÝ´<±(Á&JEKtckAíÒÊÖ═┼░@¡HÙDÆ7S5ªY8Óe',
+    8: '©.Rë╗Ø8Ýâ1▓OÚø²"iÖ`F═læºÆv┬Þ(+─┼”)s“¥ÕZáÙE@õd0¬╚Ó½;╬2 ±Ô|7ßnVù*Ï«à{þÄ║└LaWÇûr¹╔<¿pSò¡É┤:eå®X³Û▒´ó§Ðm¦­u]_j×Ã¸µ╠»▄├D╦£éôIP╣zTí!6HÈ¢Â&’?[=■,öãN~BêÌbC3g\$╩ï4ýy÷ä░c-^K┴▀│¤kèQ¯t·î\'Òðñü¼¾¨hÜÅç°fÊ┐Í}╝¶#UwÿÎÁì¦MÑúªJA59█x/YÀoq%ËG>',
+    9: '¶{ae╔╚¦¦(Ï%ÇÅj#uW[À)Ö/¡Ìi¹pJõÒ"H║│Îwæ█A`5û<zcÓ7çßYM┤P├í¢´yÈÙò§½|èn×0Of▀êÜI▒Äà®v?ã¥­R┴·ÿ}rÚNþBböôØq┐hQ]²Ñ╬ñ╦KâS┼üÁ╗“*ù!▄~▓═’d¼8£Â└╩ú”@>23┬V_CËk-\:ÍîóÞ&©TÊìð¬¸ÃXá¯¾¨µ6x─«Eg;.░$ª+╣ÆlsD^t╠¤³ZÉÕäÔý■é±Gø÷=°¿F9╝om,U º\'ÐëÝ»Lïå4Û1'
+}
+
+alphabet = list('!”’“"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ░▒▓│┤╣║╗╝┐└┴┬├─┼╚╔╩╦╠═╬█▄¦▀■')
+
+def generate_key():
+    # generates a key compatible with crunch
+    res = ''
+    thing = alphabet
+    print(len(thing))
+    for _ in range(0, 220):
+        res = res + str(thing.pop(thing.index(random.choice(thing))))
+    print(res)
+
+def crunch(word):
+    increment = 1
+    result = ''
+    try:
+        alphabet = keys.get(int(word[0]))
+    except:
+        key = random.randint(0, 9)
+        alphabet = list(keys.get(key))
+        result = str(key)
+    for letter in word:
+        index = alphabet.index(letter) + increment
+        index = index % len(alphabet)
+        result += alphabet[index]
+        increment *= 2
+    return result
+
+def uncrunch(word):
+    result = ""
+    increment = 1
+    try:
+        alphabet = keys.get(int(word[0]))
+    except:
+        key = random.randint(0, 9)
+        alphabet = keys.get(key)
+        result = str(key)
+    for letter in word[1:]:
+        index = alphabet.index(letter) - increment
+        index = index % len(alphabet)
+        result += alphabet[index]
+        increment *= 2
+    return result
--- gmaer.py
+++ gmaer.py
@@ -1,9 +1,11 @@
+# game file (hangman and Gorf)
+
 import math, random, sys, time, string
+
 from grammar_functions import *
+from crunch import *
 
-#---------------------------------------------Lists and Dictionaries---------------------------------------------#
-
-
+#---------------------------------------------Lists---------------------------------------------#
 
 pronouns = ['he', 'she', 'I', 'you', 'we', 'y\'all', 'you guys']
 verbs = ['cough', 'run', 'walk', 'defecate', 'flap', 'urinate', 'expunge', 'masticate', 'perambulate', 'touch', 'play', 'suck', 'pummel', 'open', 'spoon', 'dance', 'gorge', 'lunge', 'attack', 'jump', 'game end', 'game', 'yeet', 'clap', 'talk', 'stretch', 'work', 'swim', 'throw', 'insert', 'penetrate', 'rub', 'flex', 'slap', 'pet', 'milk', 'lick', 'call', 'prod', 'dwell', 'leap', 'strip', 'love', 'hate', 'despise', 'eat', 'fall', 'save', 'tend', 'raise', 'break', 'end', 'grow', 'obtain', 'wear', 'fight', 'fill', 'examine', 'replace', 'drink', 'hang', 'finish', 'escape', 'rid', 'adopt', 'pee', 'compact', 'ride', 'bite', 'burn', 'gibe', 'fertilize', 'vaporize', 'divorce', 'witness', 'choke', 'slam', 'ram', 'blow', 'drink', 'slurp', 'sip', 'forget', 'hurt', 'bend', 'mess', 'pay', 'see']
@@ -16,31 +18,6 @@ names = ['Betty', 'Kromas', 'Larson', 'Johannes', 'Dogman', 'Krom', 'Mikey', 'Bo
 lists = [verbs, nouns, adj, exclaim, loc, names]
 
 
-
-keys = {
-    0: '\'?IUû!wK░@ÚÏrþ¢9MÍ<ukZ:G¹Óà³á║ÈÁHïÎÔoËY┼J╠╬┴0i3²7X▓í└┤ªºv¶÷yð,µjça©* ø2▄êâ.úTpÂ’ÖØý█1ÆóÌ´╔╦[Õæb$Òä“·«åtÑã§ëßì6+;s╝g£B&Lé”▒═{EîmO°¿ü-¼¥┬èÊö5╚%Çh¾»¤q╗Þc½^×ôÐ4]"}=Ä¦(¯`▀│fD├ÛWÝS─zÀd╩Å¨Ã#ñNC┐QnÜù8/¦®ÙF­¸¡\ÉAR)¬õÿxV|_■~òP>e±l╣',
-    1: 'Ü×┼Úýæ]Z÷m±róz»¬╣êKú├wÉ³OªÇÍÛ╠╗ôÝ└BV1HdYg^Ó¶M­╬ï"vÁhX¨î>9¼┬¸ÿøÊxÄD│n´}(ET®kùIÒÆåÈûoÞc8áJsßÀí-ëqtÖ+Ð&”¦QØpÏã▀ò¥*G¯è©.05/²¿jLÂà¹$Ì§[)╚NWÙa¤Ã═3¢i°Î╝▄█Ñ{,\'`CÕ@Fä╦%A\Ô¡éâ?b4Uõ;ð#¦!<╔’|S2Rö6_░:=~■┐uË║ì7µ þ¾╩«f┤£Å·üPñ─▓º┴e▒“y½çl',
-    2: 'Õcç4Ñöa\';├G8h"[+Kó½¥Îàÿ1±>ÖÌ£7 SÙ¾qÏÝåñá’┐i²NB*x!v~í╚âIj┬/k|¨¢Ø·ïÅäÃrÂÆp{]┼P╠ÐÞþÓôs╣=ì5¶&æ▒n³êÚ█│:D“ÄßÔZ╦µXÛAC©0øõ.▄,Ç»uúë×└¹░Ò▓╗«RzJ?$╔û§E_yOT¤tW¦═%éQÍ´Mè┤Àm2@Ul¯L\¼b¬dgH■îª╬#Ë°¸V`®”▀¡─ÊoF9)eã^}wº-ùòüðYf­╝Á6<║¿(ÜÈÉ¦÷╩┴3ý',
-    3: '$╚┴})3╠ªgÚæ5"2a░óúeX¤│p▒’v┤Èy6ixSøÿç°Ò@ÔBEm*▓ï▄Î^=█²╬qu¦┐èl╦Ð¡ËÞ`R§oj¾&é7À¦-╗Í¹­º╣zQ íþîà~ZùwÁ>®ãA¿ÌÛ║{Ü4¬¸%N!ö/sL³nä├]µJÆÊ\▀¥(dÙM¨KØÇ.\'áåCUìÃrÑ:ûô╩ð«ß┬Å©Ä£Ö┼8|âõVP±GkÉñëY“╝1Óý└╔;t<¼■T+”─Ï¶×Âò,W_êüÕ¯¢[9hI´Fcb÷·?»½fH#Ý0═DO',
-    4: '┼ºCÊ¦êï?Àd¾]╩}csJØ│F▒Î▀3·á╗■iÉG# wû<«┴¥½ÒeÆÄ$*|”þ¼ýµ§!^’NÌa,óÂ÷h"ú¶├Xôb╝K┤ä╣Üì└DA­Å=ñVÞ(øÑPül9;ß©ÍI┬HM─~═0ÐÁ{¯5╔EL╦ð╠¢¨¹³QÚx-q®>ù&gRj27WÇæ“Õf╬t╚²▄¸4nBÝèÓÈéò×░»▓Umk¡\':YZõÖÏ¿Ù¤█£T´@1/åí%yªàpÔ¦v[_)¬┐ëÛu°±\8║SçzoãO+`.â6îËÿÃör',
-    5: '┼╦├ª/kËÑq7GÞæ1TÝ┴a´┤ê£┐.─Es╩ë¤ÛÇÉÓ9h)”M▒üâb└v█ÙÿñC~¿ZN³l▀Ö▄"OÁþÄ■╣o¬cD:»Ìº>d,¨P═iWASVû3║;ó¹j±©+╝ÐÅ®&¢LåúryÔ¼è5xØ“á{¸çù<õ╔¶Í=¾ÀY8▓Ã%┬·|Æ#öì6R Hà¦°^4Â¥g_¯2z\'0XµÒmôÎ«$IïF╚ä@ßUB([¡­-│píe½òt╗}Q÷\wÜÈn╠ãJuÏ░`Õ×îÚ²?¦K!ý╬*ø]ðfé’§Ê',
-    6: 'éöÈÀ╩z`j“@Z_ÉQ█Ì\©þá&NêôG¸(®▄ã\'ð-§¬]╚fü¯æ°┐ûùu;K«╠»T#dú±PàYmº╬ÿ? ┴ÊÍ¶k┬Lò├ß╗õo:¹÷0£I╔.aqpí/S4,vËx²Øñ3w[▒ë▓║¿╦hE╣½¼▀Ã+µX·’}A7ÜD*8─C2s^FªÞ¥BMÓ"”Önì5Á<1çól┼´ÑÙRJåÄ└ÅrÏ¢)¦ÇÕ6by³Û░|Ôc│O┤HWâtUÝ=­ge¡¦╝¤!VÎèÚ$%{i~¾ýïÂÐ9×äÆÒ■î¨>ø═',
-    7: '¤_bUÐhys½,Û©Lß\;ÔGWÅà^▒xÂj`*▄a¯|£╣dÉ4÷¼Oå¦▀Í6õºnô┴ip3¸ä!?ÿw=ïX1ùgø®’\'+f■P│├µ╗Ä█╦└[ãÃ×┬╝]âqz”ö▓æîì╬ÇòëRT¬ ­È«Üá┐─ú°ý┤Ë#¶-²Ql0§:.{Ú%þé“9$ñI/ü·ð)¦¢"Þ¹BVF}»êrÏ³╠2Ø¨ÎmÑ║Nv╔╩~Z¥oóû>uC¿ÌçM¾Õ╚èÀÝ´<±(Á&JEKtckAíÒÊÖ═┼░@¡HÙDÆ7S5ªY8Óe',
-    8: '©.Rë╗Ø8Ýâ1▓OÚø²"iÖ`F═læºÆv┬Þ(+─┼”)s“¥ÕZáÙE@õd0¬╚Ó½;╬2 ±Ô|7ßnVù*Ï«à{þÄ║└LaWÇûr¹╔<¿pSò¡É┤:eå®X³Û▒´ó§Ðm¦­u]_j×Ã¸µ╠»▄├D╦£éôIP╣zTí!6HÈ¢Â&’?[=■,öãN~BêÌbC3g\$╩ï4ýy÷ä░c-^K┴▀│¤kèQ¯t·î\'Òðñü¼¾¨hÜÅç°fÊ┐Í}╝¶#UwÿÎÁì¦MÑúªJA59█x/YÀoq%ËG>',
-    9: '¶{ae╔╚¦¦(Ï%ÇÅj#uW[À)Ö/¡Ìi¹pJõÒ"H║│Îwæ█A`5û<zcÓ7çßYM┤P├í¢´yÈÙò§½|èn×0Of▀êÜI▒Äà®v?ã¥­R┴·ÿ}rÚNþBböôØq┐hQ]²Ñ╬ñ╦KâS┼üÁ╗“*ù!▄~▓═’d¼8£Â└╩ú”@>23┬V_CËk-\:ÍîóÞ&©TÊìð¬¸ÃXá¯¾¨µ6x─«Eg;.░$ª+╣ÆlsD^t╠¤³ZÉÕäÔý■é±Gø÷=°¿F9╝om,U º\'ÐëÝ»Lïå4Û1'
-}
-
-alphabet = list('\'?IUû!wK░@ÚÏrþ¢9MÍ<ukZ:G¹Óà³á║ÈÁHïÎÔoËY┼J╠╬┴0i3²7X▓í└┤ªºv¶÷yð,µjça©* ø2▄êâ.úTpÂ’ÖØý█1ÆóÌ´╔╦[Õæb$Òä“·«åtÑã§ëßì6+;s╝g£B&Lé”▒═{EîmO°¿ü-¼¥┬èÊö5╚%Çh¾»¤q╗Þc½^×ôÐ4]"}=Ä¦(¯`▀│fD├ÛWÝS─zÀd╩Å¨Ã#ñNC┐QnÜù8/¦®ÙF­¸¡\ÉAR)¬õÿxV|_■~òP>e±l╣')
-
-'''
-thing = '!”’“"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ░▒▓│┤╣║╗╝┐└┴┬├─┼╚╔╩╦╠═╬█▄¦▀■'
-res = ''
-thing = list(thing)
-print(len(thing))
-for _ in range(0, 220):
-    res = res + str(thing.pop(thing.index(random.choice(thing))))
-print(res)
-'''
 
 #---------------------------------------------Non-Gorf (functions)---------------------------------------------#
 
@@ -108,38 +85,6 @@ def hangman(word=None, guesses_to_lose=10):
         print("\nCorrectly Guessed Letters:", correct_letters)
         print("\nCorrectly Guessed Letters:", incorrect_letters)
         print("Wrong Guesses:", guesses, "/", guesses_to_lose)
-
-def crunch(word):
-    increment = 1
-    result = ''
-    try:
-        alphabet = keys.get(int(word[0]))
-    except:
-        key = random.randint(0, 9)
-        alphabet = list(keys.get(key))
-        result = str(key)
-    for letter in word:
-        index = alphabet.index(letter) + increment
-        index = index % len(alphabet)
-        result += alphabet[index]
-        increment *= 2
-    return result
-
-def uncrunch(word):
-    result = ""
-    increment = 1
-    try:
-        alphabet = keys.get(int(word[0]))
-    except:
-        key = random.randint(0, 9)
-        alphabet = keys.get(key)
-        result = str(key)
-    for letter in word[1:]:
-        index = alphabet.index(letter) - increment
-        index = index % len(alphabet)
-        result += alphabet[index]
-        increment *= 2
-    return result
 
 #---------------------------------------------Gorf---------------------------------------------#
 
@@ -262,22 +207,22 @@ class Gorf(object):
         self.seed = random.randint(0, self.max_seed)  
     
     def save_sentence(self):
-        with open("saves/gorf_favs.txt", 'a') as save_file:
-            save_file.write(self.sentence)
+        with open(self.save_file, 'a') as sf:
+            sf.write(self.sentence)
 
     def get_saved_sentences(self):
-        with open('saves/gorf_favs.txt', 'r') as save_file:
-            data = save_file.read()
+        with open(self.save_file, 'r') as sf:
+            data = sf.read()
         return data
 
     def erase_saved_data(self):
-        with open('saves/gorf_favs.txt', 'w') as save_file:
-            save_file.write('')
+        with open(self.save_file, 'w') as sf:
+            sf.write('')
 
 #---------------------------------------------Execution---------------------------------------------#
     
 
-def main():
+def main(): # premade game using gorf and other functions
     # constants unique to main
     affirmations = ['yes', 'y', 'yeah', 'ye', 'ys', 'i do', 'indeed', 'affirmative', 'ok'] # any string that is considered a confirmation/approval
 
@@ -343,7 +288,4 @@ def main():
             using = False
 
 if __name__ == '__main__':
-    test = str(input('choice: '))
-    print(present(test))
-    
     main()
