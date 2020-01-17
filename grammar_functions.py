45da6f7cfdf8a7a479b5832ad25c50a52b218d52
dirkwind <dirkwind707@gmail.com>
2020-01-17 00:14:26 -0600

    improve grammar_functions.py

--- grammar_functions.py
+++ grammar_functions.py
@@ -4,11 +4,10 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 grammar_exceptions = {
     #template: 'word' : {'plural' : '', 'past' : '', 'present' : ''},
     'run' : {'past' : 'ran'},
-    'swim' : {'past' : 'swam'},
-    'pet' : {'past' : 'pat'},
     'ride' : {'past' : 'rode'},
     'drink' : {'past' : 'drank'},
     'fight' : {'past' : 'fought'},
+    'bring' : {'past' : 'brought'},
     'bite' : {'past' : 'bit'},
     'wear' : {'past' : 'wore'},
     'woman' : {'plural' : 'women'},
@@ -16,8 +15,8 @@ grammar_exceptions = {
 }
 
 def in_exeptions(word, tense):
-    if word in grammar_exceptions.keys():
-        if tense in grammar_exceptions[word].keys():
+    if word in grammar_exceptions:
+        if tense in grammar_exceptions[word]:
             return True
     return False
 
@@ -52,7 +51,11 @@ def past(word):
         return word + 'd'
     elif scnd_last_letter in vowels and word[len(word) - 3] in vowels:
         return word + 'ed'
-    elif last_letter not in set(['e', 'i', 'o', 'u', 'y', 'a', 'k', 't', 'x', 'h', 'w', 'd', 'y']):
+    elif scnd_last_letter == 'e' and last_letter not in vowels:
+        return word[:len(word) - 2] + 'o' + last_letter
+    elif scnd_last_letter in {'i', 'e'} and last_letter not in vowels:
+        return word[:len(word) - 2] + 'a' + last_letter
+    elif last_letter not in set(['e', 'i', 'o', 'u', 'y', 'a', 'k', 'x', 'h', 'w', 'd', 'y']):
         return word + last_letter + 'ed'
     elif last_letter == 'y' and scnd_last_letter not in vowels:
         return word[:len(word) - 1] + 'ied'
@@ -85,9 +88,9 @@ def nounify(word):
 
 def add_a(word):
     if word[0] in vowels:
-        return 'an ' + str(word)
+        return 'an ' + word
     else:
-        return 'a ' + str(word)
+        return 'a ' + word
 
 def pronoun_possesive(pronoun):
     if pronoun in ['she', 'he']:
